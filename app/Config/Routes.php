bfc6a8a8f2cc3141cce99253f02a75355e0d5269
zopvan <zooganz1@gmail.com>
2026-04-08 14:37:10 +0700

    mvc

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/product', 'ProductController::index');
+$routes->get('/keranjang', 'TransaksiController::index');
---
+++ app/Views/v_home.php
@@ -0,0 +1,4 @@
+ini halaman home<br>
+<a href="/produk">ke halaman produk</a><br>
+<a href="/keranjang">ke halaman keranjang</a>
+
