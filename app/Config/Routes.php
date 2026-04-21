a0a365188f379c9df41dad4d6a4556957c1ea1fe
zopvan <zooganz1@gmail.com>
2026-04-21 23:23:03 +0700

    capstone4 session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
 $routes->get('/product', 'ProductController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -173,7 +173,8 @@
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                     <img src="<?= base_url() ?>NiceAdmin/assets/img/profile-img.jpg" alt="Profile"
                         class="rounded-circle">
-                    <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                    <span class="d-none d-md-block dropdown-toggle ps-2"><?= session()->get('username'); ?>
+                        (<?= session()->get('role'); ?>)</span>
                 </a><!-- End Profile Iamge Icon -->
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -216,7 +217,7 @@
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
+                        <a class="dropdown-item d-flex align-items-center" href="logout">
                             <i class="bi bi-box-arrow-right"></i>
                             <span>Sign Out</span>
                         </a>
