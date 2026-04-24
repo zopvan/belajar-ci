d0b40f80b17bccea080849b48550f2a508beb3a2
zopvan <zooganz1@gmail.com>
2026-04-24 21:58:07 +0700

    filter update dikit

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -23,7 +23,7 @@
             <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'product') ? "" : "collapsed" ?>" href="product">
                     <i class="bi bi-receipt"></i>
-                    <span>Produk</span>
+                    <span>Product</span>
                     </a>
         <?php
         }
